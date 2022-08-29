6eed38784f6c0ccde3be982b2b612479cc852858
Nikita <trabelsi@inbox.ru>
2022-08-29 18:23:47 +0300

    -initial db

---
+++ db/scripts/create.sql
@@ -0,0 +1,11 @@
+create database passport;
+
+create table passports (
+    id serial primary key,
+    series int not null,
+    number int not null,
+    expired timestamp,
+    surname varchar(500) not null,
+    firstname varchar(500) not null,
+    address varchar(2000)
+)
